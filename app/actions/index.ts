160aa57b962a7f2f820f606af2fbe392520cabb9
Muhammad15246 <faizybangash05@gmail.com>
2025-08-11 13:08:40 +0500

    add-google-aauth-server

---
+++ app/actions/index.ts
@@ -0,0 +1,12 @@
+'use server';
+
+export const socialLogin = async (formdata:any) => {
+    try {
+    const action = formdata.get('action');
+    console.log(action);
+        
+    } catch (error) {
+        
+    }
+    
+};
