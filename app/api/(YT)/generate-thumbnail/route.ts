a0f509490211be4a4cb2f435d827d0f0e875be94
FAIZY152 <fayazjani7576@gmail.com>
2025-09-12 12:47:54 +0500

    add-input-data-yt-thumbnail

--- app/api/(YT)/generate-thumbnail/route.ts
+++ app/api/(YT)/generate-thumbnail/route.ts
@@ -1,12 +1,26 @@
 import { NextRequest } from "next/server";
+import { auth } from "@/auth";
+
 export const POST = async (req: NextRequest) => {
   try {
+    const session = await auth();
+    if (!session?.user) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
     const formData = await req.formData();
     const userinpt = formData.get("userinpt")?.toString() || "";
     const refrenceImg = formData.get("refrenceImg") || null;
     const faceImg = formData.get("faceImg") || null;
 
-    console.log(userinpt, refrenceImg, faceImg);
+    const inputData = {
+      userinpt: userinpt,
+      refrenceImg: refrenceImg
+        ? await GetBufferData(refrenceImg as File)
+        : null,
+      faceImg: faceImg ? await GetBufferData(faceImg as File) : null,
+      userEmail: session.user.email,
+      userId: session.user.id,
+    };
   } catch (error) {
     console.error("Error processing request:", error);
   }
