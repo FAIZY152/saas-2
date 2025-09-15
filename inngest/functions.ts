729826dccf20c494965c83a8858b77bc85a78b8e
FAIZY152 <fayazjani7576@gmail.com>
2025-09-15 11:35:04 +0500

    add-generate-thumbnail-inggest-function

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -8,3 +8,13 @@ export const helloWorld = inngest.createFunction(
     return { message: `Hello ${event.data.email}!` };
   }
 );
+
+export const GenerateAiThumbnail = inngest.createFunction(
+  { id: "ai/generate-thumbnail" },
+  { event: "ai/generate-thumbnail" },
+  async ({ event, step }) => {
+    // TODO: Implement thumbnail generation logic here
+    const {} = event.data;  
+    return { message: "AI thumbnail generated." };
+  }
+);
