d30328058aeaba0da924d8ccd836c0c768f21787
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2025-12-04 00:28:51 +0500

    add homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/components/pages/Main/Home";
 
 export default function MainPage() {
   return (
-    // adding brevo for mailing
+    // add homepage
     <div className="min-h-screen bg-background">
       <Home />
     </div>
