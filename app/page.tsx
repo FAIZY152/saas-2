7599d6247ee3c52236106f50861aa20ee9fe5e82
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2025-12-04 00:28:27 +0500

    adding brevo for mailing

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Home from "@/components/pages/Main/Home";
 
 export default function MainPage() {
   return (
+    // adding brevo for mailing
     <div className="min-h-screen bg-background">
       <Home />
     </div>
