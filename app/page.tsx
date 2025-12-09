bd7d76c13013444edbc642a812f0b46b1a772037
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2025-12-09 16:38:27 +0500

    Add comment in MainPage component
    Added a comment in the MainPage component.

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Home from "@/components/pages/Main/Home";
 
 export default function MainPage() {
   return (
+    // hello 
     <div className="min-h-screen bg-background">
       <Home />
     </div>
