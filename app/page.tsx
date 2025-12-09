3ddc6bd3c6567729ed52a7602393acb30ad5b11b
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2025-12-09 16:37:59 +0500

    Remove comment from MainPage component
    Removed comment about cover landing from MainPage component.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Home from "@/components/pages/Main/Home";
 
 export default function MainPage() {
   return (
-    // cover landing
     <div className="min-h-screen bg-background">
       <Home />
     </div>
