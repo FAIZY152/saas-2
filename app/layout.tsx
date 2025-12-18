f641389d89f94d952c05d13334af2ef86cafa3c2
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2025-12-19 01:04:46 +0500

    testing kafka for message broker

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import { Suspense } from "react";
 import { LoadingSkeleton } from "@/components/ui/loading";
 import { HydrateStore } from "@/components/HydrateStore";
 
-// Optimizd adding redis
+// Optimizd adding kafka
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
