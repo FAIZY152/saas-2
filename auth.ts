cd5c0f61f11634896e9c9355c30c4c2c6783c69f
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2025-12-23 03:32:24 +0500

    aws ses for mailing

--- auth.ts
+++ auth.ts
@@ -4,7 +4,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { AuthDB } from "@/lib/auth-db";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  // add better authentication
+  // add better
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
