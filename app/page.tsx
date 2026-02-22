c1646ad8384bbfc2977c9bf7b619b4d380f09022
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-02-22 12:12:24 +0500

    replace ses with brevo

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/components/pages/Main/Home";
 
 export default function MainPage() {
   return (
-    // hello sir
+    // add brevo
     <div className="min-h-screen bg-background">
       <Home />
     </div>
