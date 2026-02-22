39c8bc59b1b59c95440d6f80fa2d44542c014eee
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-02-22 12:12:51 +0500

    add better auth

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/components/pages/Main/Home";
 
 export default function MainPage() {
   return (
-    // add brevo
+    // add rabit mq
     <div className="min-h-screen bg-background">
       <Home />
     </div>
