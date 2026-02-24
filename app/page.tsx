510bb2b70ebee52a396d862a8ec39ffb7135b7d7
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-02-24 12:08:23 +0500

    // add rabit mq

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/components/pages/Main/Home";
 
 export default function MainPage() {
   return (
-    // add rabit 
+    // add rabit mq
     <div className="min-h-screen bg-background">
       <Home />
     </div>
