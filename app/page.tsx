14c85ac677fd19085e68bbf6323a3a5ea83f4233
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-03-30 13:35:33 +0500

    reflect payment-flow addition

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/components/pages/Main/Home";
 
 export default function MainPage() {
   return (
-    // add rabit-mq for bg tasks
+    // add payment-flow
     <div className="min-h-screen bg-background">
       <Home />
     </div>
