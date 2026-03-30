47a4a9258c632e95e57f96c726ce5dc0cb1beea4
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-03-30 13:37:22 +0500

    add stripe for payment section

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/components/pages/Main/Home";
 
 export default function MainPage() {
   return (
-    // add stripe 
+    // add stripe for payment section
     <div className="min-h-screen bg-background">
       <Home />
     </div>
