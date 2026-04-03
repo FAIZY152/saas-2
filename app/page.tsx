cb98dc4d1a8e882ce5e4ce4ae57eb7a85bc029da
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-04-03 23:14:45 +0500

    added-organization-flow

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/components/pages/Main/Home";
 
 export default function MainPage() {
   return (
-    // add stripe for payment
+    // added-organization-flow
     <div className="min-h-screen bg-background">
       <Home />
     </div>
