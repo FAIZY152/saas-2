2f65e5981e867392f3343aafce1e2553c6ca58da
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-04-13 18:33:18 +0500

    added-rabit-mq-for bg-tasks

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/components/pages/Main/Home";
 
 export default function MainPage() {
   return (
-    // multi-tenate-schema
+    // added-rabit-mq-for bg-tasks
     <div className="min-h-screen bg-background">
       <Home />
     </div>
