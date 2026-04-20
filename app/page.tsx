3005ad0f22563e9d8c9d340752f693629764ab57
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-04-20 22:23:00 +0500

    help-section-form

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/components/pages/Main/Home";
 
 export default function MainPage() {
   return (
-    // added-help-section
+    // added-help-section-form
     <div className="min-h-screen bg-background">
       <Home />
     </div>
