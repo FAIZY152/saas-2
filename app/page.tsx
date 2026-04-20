40e1583344c178ab72206634b45a3ca1179c8496
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-04-20 22:24:16 +0500

    setup-onboarding-flow-added-with-bussines-logic

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/components/pages/Main/Home";
 
 export default function MainPage() {
   return (
-    // setup-onboarding-flowadded
+    // setup-onboarding-flow-added-with-bussines-logic
     <div className="min-h-screen bg-background">
       <Home />
     </div>
