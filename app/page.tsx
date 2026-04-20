62fe6a5e1c946f947f7f9b55b4e49cd568e5f445
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-04-20 22:22:43 +0500

    added-help-section

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/components/pages/Main/Home";
 
 export default function MainPage() {
   return (
-    // added-rabit-mq
+    // added-help-section
     <div className="min-h-screen bg-background">
       <Home />
     </div>
