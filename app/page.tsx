125b6922ee15865eabb63174a20e7a02923e286a
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-05-05 00:15:44 +0500

    fix-a-s3-url-issue-hide-from-user

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/components/pages/Main/Home";
 
 export default function MainPage() {
   return (
-    // add-a-proxy-filder
+    // 
     <div className="min-h-screen bg-background">
       <Home />
     </div>
