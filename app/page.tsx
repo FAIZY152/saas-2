5123de4acf16fd8f0bbf015250bcaffa300dd1a6
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-05-05 00:09:38 +0500

    setup-download-option

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/components/pages/Main/Home";
 
 export default function MainPage() {
   return (
-    // setup-onboarding-flow-added-with-bussines-logic
+    // setup-download-option
     <div className="min-h-screen bg-background">
       <Home />
     </div>
