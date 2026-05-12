7d6982a743501a5f156d70ae0614b44f81363083
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-05-12 19:44:40 +0500

    design a Structure Elvation page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/components/pages/Main/Home";
 
 export default function MainPage() {
   return (
-    // design a blog page
+    // design a Structure Elvation page
     <div className="min-h-screen bg-background">
       <Home />
     </div>
