db376b71a247d522dcabd169ff4499cccd7e8270
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-05-22 17:48:49 +0500

    integrate a professional flow

--- auth.ts
+++ auth.ts
@@ -4,7 +4,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { AuthDB } from "@/lib/auth-db";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  // integrate a organization flow
+  // integrate a professional flow
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
