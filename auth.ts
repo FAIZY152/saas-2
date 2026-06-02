113d6e17b9f6f4a407c4af0c6e2f515df7718ca5
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-06-03 01:01:00 +0500

    include a notification module in dashboar

--- auth.ts
+++ auth.ts
@@ -4,7 +4,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { AuthDB } from "@/lib/auth-db";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  // include payment module in dashboard 
+  // include a notification module in dashboard 
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
