502eead8b466c91a68fc7d4066cd3125f9c9956d
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-06-04 01:03:28 +0500

    design a new pages

--- auth.ts
+++ auth.ts
@@ -4,7 +4,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { AuthDB } from "@/lib/auth-db";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  // add team member module
+  // design a new pages
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
