ffcbf53684a1654ad2307f64d8728f4410bec5b6
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-06-11 22:12:19 +0500

    design a token flowdesign a token flow

--- auth.ts
+++ auth.ts
@@ -4,7 +4,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { AuthDB } from "@/lib/auth-db";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  // design a new pages
+  // design a token flow
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
