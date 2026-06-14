5dba0450d939b93a36449e10d721d9d8cca51057
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-06-14 20:27:15 +0500

    complete a email integration with brevo

--- auth.ts
+++ auth.ts
@@ -4,7 +4,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { AuthDB } from "@/lib/auth-db";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  // complete a blocker flow in milestone 5
+  // complete a email integration with brevo
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
