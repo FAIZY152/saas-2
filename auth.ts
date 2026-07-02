32b455e8cc4865260cd163b8505d563a5e72389f
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-07-02 17:42:45 +0500

    Remove Modal flow  and seperate the both request

--- auth.ts
+++ auth.ts
@@ -4,7 +4,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { AuthDB } from "@/lib/auth-db";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  // Handle organization flow
+  // Remove Modal flow  and seperate the both request
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
