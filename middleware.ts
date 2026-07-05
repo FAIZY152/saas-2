2aba6a6927f67c4ced48d62ed37480b888a71d6d
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-07-05 19:08:41 +0500

    add a Google auth

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ export function middleware(req: NextRequest) {
   const response = NextResponse.next();
   
   // Security headers
-  // add a social login 
+  // add a Google auth
   response.headers.set('X-Frame-Options', 'DENY');
   response.headers.set('X-Content-Type-Options', 'nosniff');
   response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
