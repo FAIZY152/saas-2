56e220b849949db70bf58f305961a077004b45cf
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-07-20 13:38:49 +0500

    Solve A Pattern Loop

--- auth.ts
+++ auth.ts
@@ -4,7 +4,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { AuthDB } from "@/lib/auth-db";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  // add a Migration flow through Synchronization
+  // Solve A Pattern Loop
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
